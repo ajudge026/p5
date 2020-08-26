6434c5b31dc26dfe578abacb6a7c1aca24891cae
ajudge026 <67866986+ajudge026@users.noreply.github.com>
2020-08-26 19:20:23 -0400

    Add files via upload

--- Core.c
+++ Core.c
@@ -493,7 +493,7 @@ Signal forwarding_unit(Signal *Forward_A,
 	
 	 if(M_reg_load.signals.RegWrite &&
 	(M_reg_load.write_reg != 0) &&
-	(~(E_reg_load.RegWrite && (E_reg_load.write_reg != 0)) &&
+	~((E_reg_load.RegWrite && (E_reg_load.write_reg != 0)) &&
 	((E_reg_load.write_reg == ID_reg_load.reg_read_index_1)) ) &&
 	(M_reg_load.write_reg == ID_reg_load.reg_read_index_1)
 	)
@@ -503,7 +503,7 @@ Signal forwarding_unit(Signal *Forward_A,
 	
 	if(M_reg_load.signals.RegWrite &&
 	(M_reg_load.write_reg != 0) &&
-	(~(E_reg_load.RegWrite && (E_reg_load.write_reg != 0)) &&
+	~((E_reg_load.RegWrite && (E_reg_load.write_reg != 0)) &&
 	((E_reg_load.write_reg == ID_reg_load.reg_read_index_2))) &&
 	(M_reg_load.write_reg == ID_reg_load.reg_read_index_2)
 	)
