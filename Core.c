d5fe1ff8aa1d641445c244bfffa7ac7da6bc8a7d
ajudge026 <67866986+ajudge026@users.noreply.github.com>
2020-08-26 17:40:06 -0400

    Add files via upload

--- Core.c
+++ Core.c
@@ -494,7 +494,7 @@ Signal forwarding_unit(Signal *Forward_A,
 	printf("%s = %ld\n",VariableName(E_reg_load.RegWrite),E_reg_load.RegWrite);
 	printf("%s = %ld\n",VariableName(E_reg_load.write_reg),E_reg_load.write_reg);
 	printf("%s = %ld\n",VariableName(ID_reg_load.reg_read_index_1),ID_reg_load.reg_read_index_1);
-	printf("%s = %ld\n",VariableName(ID_reg_load.reg_read_index_1),ID_reg_load.reg_read_index_1);
+	printf("%s = %ld\n",VariableName(ID_reg_load.reg_read_index_2),ID_reg_load.reg_read_index_2);
 	if(M_reg_load.signals.RegWrite &&
 	(M_reg_load.write_reg != 0) &&
 	~(E_reg_load.RegWrite && (E_reg_load.write_reg != 0)) &&
