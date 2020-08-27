8712f3e937fda249541c3f9b7eed551d62b9ec89
ajudge026 <67866986+ajudge026@users.noreply.github.com>
2020-08-27 12:48:05 -0400

    Add files via upload

--- Core.c
+++ Core.c
@@ -45,9 +45,9 @@ bool tickFunc(Core *core)
 	if( (core->stages_complete < (num_instructions )))
 	{
 		
-		core->IF_reg.PC = core->PC; 
-		if(PC_Control == 0)core->PC = core->PC + 4;	
+		core->IF_reg.PC = core->PC; 		
 		core->IF_reg.instruction = core->instr_mem->instructions[core->PC / 4].instruction;	
+		if(PC_Control == 0)core->PC = core->PC + 4;	
 		 core->IF_reg.reg_read_index_1 = (core->IF_reg.instruction >> (7 + 5 + 3)) & 31;
 		core->IF_reg.reg_read_index_2 = (core->IF_reg.instruction >> (7 + 5 + 3 + 5)) & 31;		
 	}
