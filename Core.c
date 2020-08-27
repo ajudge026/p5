da7a0c7ee769c720db3e037e1e367a9abd5d0671
ajudge026 <67866986+ajudge026@users.noreply.github.com>
2020-08-27 08:35:10 -0400

    Add files via upload

--- Core.c
+++ Core.c
@@ -512,8 +512,8 @@ void hazard_unit(	Signal *PC_Control,
 	}
 }
 	
-void testing_function(char testing[], Byte *data_mem[1024], // data memor,
-    Register *reg_file[32])
+void testing_function(char testing[], Byte *data_mem, 
+    Register *reg_file)
 {
 	if (strcmp(testing, "p4") == 0)
 	{
